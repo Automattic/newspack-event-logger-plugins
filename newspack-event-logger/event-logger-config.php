e0eba77cfa0bf90514dec93c0c7477d6f6e6b481
Christopher Reaume <christopher.reaume@automattic.com>
2026-04-11 16:56:16 -0700

    fix: default config

--- newspack-event-logger/event-logger-config.php
+++ newspack-event-logger/event-logger-config.php
@@ -25,7 +25,7 @@ return [
 	// - Deleted/recreated users with the same username regain access.
 	// - Renamed users lose access (update this list after username changes).
 	// - For stronger authorization, consider switching to user IDs in future.
-	'allowed_users'          => [ 'adminnewspack', 'dispatch' ],
+	'allowed_users'          => [],
 
 	// Enable/disable logging.
 	'enable_logging'         => true,
